c78c9f3b141eccf1313a6ba644870e7de77ea9ae
Priyam <priyam009@live.com>
2020-04-05 17:10:42 +1000

    Add input field to search monsters

--- src/App.js
+++ src/App.js
@@ -20,7 +20,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters}/>
+        <input
+          type="search"
+          placeholder="search monsters"
+          onChange={e => this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={this.state.monsters} />
       </div>
     );
   }
