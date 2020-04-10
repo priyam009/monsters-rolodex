7454c35c248f52ce1f2c15d1dd84f5e26d59e5de
Priyam <priyam009@live.com>
2020-04-10 22:10:24 +1000

    Adding files for githib pages

---
+++ build/precache-manifest.8986f7abe4aefab6d4ad6aa1bae4df62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d444b6bb8f22d607a856ff0a95b83cb8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cf09e752d3ef17d75478",
+    "url": "/monsters-rolodex/static/css/main.ed9cd123.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf09e752d3ef17d75478",
+    "url": "/monsters-rolodex/static/js/main.44c3550d.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.52e059c0df1bca82ce495b85802a3def.js"
+  "/monsters-rolodex/precache-manifest.8986f7abe4aefab6d4ad6aa1bae4df62.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/monsters-rolodex/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
