b7120056c61218127d6eeb8db63c9c8823a28b16
Priyam <priyam009@live.com>
2020-04-10 21:53:32 +1000

    Add build

---
+++ build/precache-manifest.52e059c0df1bca82ce495b85802a3def.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1254fbd3ffbd7d057ac10597be197a29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45fc2dee13fca8643d86",
+    "url": "/static/css/main.ed9cd123.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45fc2dee13fca8643d86",
+    "url": "/static/js/main.ed695333.chunk.js"
+  },
+  {
+    "revision": "ad591d0f4708f88e02d5",
+    "url": "/static/js/runtime-main.ca69820c.js"
+  }
+]);
