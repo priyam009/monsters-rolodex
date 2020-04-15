edcc1e9a20f5f5dae2d827650a4279a243c5f69c
Priyam <priyam009@live.com>
2020-04-15 20:14:54 +1000

    Update the card arrangement and keep centered

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -8,8 +8,8 @@ export const Card = props => {
         alt="monster"
         src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
       ></img>
-      <h2>{props.monster.name}</h2>
-      <h2>{props.monster.email}</h2>
+        <h2>{props.monster.name}</h2>
+        <h2>{props.monster.email}</h2>
     </div>
   );
 };
